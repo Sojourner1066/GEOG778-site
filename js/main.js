23e06fbd1caee3e63f7ec01df4aa18df1aa869bd
Sojourner1066 <morgangrundy@gmail.com>
2025-03-08 18:09:06 +0000

    updated the style

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@
 document.body.innerHTML = '<div id="map"></div>';
 
 const map = L.map('map').setView([2.5, 20.0], 5);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/icecream-v1/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/1/0/0.mvt?access_token=pk.eyJ1IjoibXdpc2NnIiwiYSI6ImNtN2VtbGEzNzBnaTgyam9vZXl3YzM2Ym4ifQ.YJe4CFT-CEYhl0D98Wk8aw"
+L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXdpc2NnIiwiYSI6ImNtN2VtbGEzNzBnaTgyam9vZXl3YzM2Ym4ifQ.YJe4CFT-CEYhl0D98Wk8aw', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     tileSize: 512,
     zoomOffset: -1,
@@ -27,3 +27,15 @@ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/icecream-v1/tiles/{z}/{x}/{
 L.marker([2.5, 20.0]).addTo(map)
     .bindPopup('Geographic center of Africa')
     .openPopup();
+
+
+// Initialize the map
+// var map = L.map('map').setView([0, 0], 2); // Set to your preferred center and zoom
+
+// // Add Mapbox Light as a tile layer
+// L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=YOUR_MAPBOX_ACCESS_TOKEN', {
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     attribution: '© <a href="https://www.mapbox.com/">Mapbox</a> © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     accessToken: 'YOUR_MAPBOX_ACCESS_TOKEN'
+// }).addTo(map);

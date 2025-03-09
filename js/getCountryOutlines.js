504a57c355ce528b622d78974383ef5a28676838
Sojourner1066 <morgangrundy@gmail.com>
2025-03-09 13:54:21 +0000

    trying to get polygons for countries

---
+++ js/getCountryOutlines.js
@@ -0,0 +1,66 @@
+async function fetchWikidataGeoShapes() {
+    const endpointUrl = "https://query.wikidata.org/sparql";
+    const sparqlQuery = `
+        SELECT ?country ?countryLabel ?iso3166_3 ?geoShape WHERE {
+          ?country wdt:P31 wd:Q3624078;  # Instance of sovereign state
+                   wdt:P3896 ?geoShape;  # Has GeoShape property
+                   wdt:P298 ?iso3166_3.  # ISO 3166-1 alpha-3 code
+          SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+        }
+        ORDER BY ?iso3166_3
+    `;
+
+    const fullUrl = endpointUrl + "?query=" + encodeURIComponent(sparqlQuery) + "&format=json";
+
+    try {
+        const response = await fetch(fullUrl, { headers: { "Accept": "application/json" } });
+        if (!response.ok) throw new Error("Failed to fetch SPARQL results");
+
+        const data = await response.json();
+        return data.results.bindings.map(item => ({
+            iso3166_3: item.iso3166_3.value,
+            geoShape: item.geoShape.value.replace("http://commons.wikimedia.org/data/main/", "Data:") // Format properly
+        }));
+    } catch (error) {
+        console.error("Error fetching Wikidata GeoShapes:", error);
+        return [];
+    }
+}
+
+async function mergeGeoShapes() {
+    const countries = await fetchWikidataGeoShapes();
+
+    let mergedGeoJSON = {
+        type: "FeatureCollection",
+        features: []
+    };
+
+    for (const country of countries) {
+        const geoShapeUrl = `https://commons.wikimedia.org/wiki/Special:EntityData/${country.geoShape}.geojson`;
+
+        try {
+            const response = await fetch(geoShapeUrl);
+            if (!response.ok) throw new Error(`Failed to fetch ${geoShapeUrl}`);
+
+            const geojsonData = await response.json();
+
+            if (geojsonData.type === "FeatureCollection") {
+                geojsonData.features.forEach(feature => {
+                    feature.properties = { ...feature.properties, iso3166_3: country.iso3166_3 };
+                    mergedGeoJSON.features.push(feature);
+                });
+            } else if (geojsonData.type === "Feature") {
+                geojsonData.properties = { ...geojsonData.properties, iso3166_3: country.iso3166_3 };
+                mergedGeoJSON.features.push(geojsonData);
+            }
+
+        } catch (error) {
+            console.error(`Error fetching GeoJSON for ${country.iso3166_3}:`, error);
+        }
+    }
+
+    return mergedGeoJSON;
+}
+
+// Export the function for use in your HTML
+export { mergeGeoShapes };
